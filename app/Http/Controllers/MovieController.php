bdf00e56a92b09c0d9f8a26b4cc9d3c580c8aa65
manfred-commits <manfredipiraino@gmail.com>
2021-11-08 16:10:52 +0100

    added access to the Db in MovieController

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Movie;
 
 class MovieController extends Controller
 {
     public function index()
     {
+        $paste = Movie::all();
+        dd($paste);
         return view('home');
     }
 }
