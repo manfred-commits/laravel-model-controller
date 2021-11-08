375965639bff6bb31e1759d9e8fa91c8ad83a375
manfred-commits <manfredipiraino@gmail.com>
2021-11-08 16:23:55 +0100

    fine esercizio base

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,10 +4,14 @@
 
 @section('pageContent')
     <main>
+        
         @foreach ($movies as $movie)
             <div class="container-movie">
-                <h2>{{$movie['title']}}</h2>
-
+                <h2>Titolo: {{$movie['title']}}</h2>
+                <h2> Titolo Originale: {{$movie['original_title']}}</h2>
+                <h2>Nazionalità: {{$movie['nationality']}}</h2>
+                <h2>Data d'uscita: {{$movie['date']}}</h2>
+                <h2>Punteggio medio: {{$movie['vote']}}</h2>
             </div>
             
         @endforeach
