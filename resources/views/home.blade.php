b3949f5ddb898b236309a2d189fd304bae43c56c
manfred-commits <manfredipiraino@gmail.com>
2021-11-08 16:44:41 +0100

    added style to the page 'fine es bonus'

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,17 +3,20 @@
 
 
 @section('pageContent')
+<section class="container-content">
     <main>
-        
+        <h1>Movies</h1>
         @foreach ($movies as $movie)
             <div class="container-movie">
                 <h2>Titolo: {{$movie['title']}}</h2>
-                <h2> Titolo Originale: {{$movie['original_title']}}</h2>
-                <h2>Nazionalità: {{$movie['nationality']}}</h2>
-                <h2>Data d'uscita: {{$movie['date']}}</h2>
-                <h2>Punteggio medio: {{$movie['vote']}}</h2>
+                <h3> Titolo Originale: {{$movie['original_title']}}</h3>
+                <h4>Nazionalità: {{$movie['nationality']}}</h4>
+                <h5>Data d'uscita: {{$movie['date']}}</h5>
+                <h6>Punteggio medio: {{$movie['vote']}}</h6>
             </div>
             
         @endforeach
     </main>
+
+</section>
 @endsection
