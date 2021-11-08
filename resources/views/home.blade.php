6b98fc5951f662ec3ed28fc848f7f10583f0a173
manfred-commits <manfredipiraino@gmail.com>
2021-11-08 17:04:57 +0100

    fixed descriptions in same language

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,11 +8,11 @@
         <h1>Movies</h1>
         @foreach ($movies as $movie)
             <div class="container-movie">
-                <h2>Titolo: {{$movie['title']}}</h2>
-                <h3> Titolo Originale: {{$movie['original_title']}}</h3>
-                <h4>Nazionalità: {{$movie['nationality']}}</h4>
-                <h5>Data d'uscita: {{$movie['date']}}</h5>
-                <h6>Punteggio medio: {{$movie['vote']}}</h6>
+                <h2>Title: {{$movie['title']}}</h2>
+                <h3> Original Title: {{$movie['original_title']}}</h3>
+                <h4>Nationality: {{$movie['nationality']}}</h4>
+                <h5>Release Date: {{$movie['date']}}</h5>
+                <h6>Average Vote: {{$movie['vote']}}</h6>
             </div>
             
         @endforeach
