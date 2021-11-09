4a25f0e7dcc2196af6d67c9400b40a8be5a762a2
manfred-commits <manfredipiraino@gmail.com>
2021-11-09 10:56:27 +0100

    added fix to card witdth

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,10 +8,10 @@
         <h1>Movies DB</h1>
         @foreach ($movies as $movie)
             <div class="container-movie">
-                <h2>Title: {{$movie['title']}}</h2>
-                <h3> Original Title: {{$movie['original_title']}}</h3>
-                <h4>Nationality: {{strtoupper($movie['nationality'])}}</h4>
-                <h5>Release Date: {{$movie['date']}}</h5>
+                <h2>Title: <br>{{$movie['title']}}</h2>
+                <h3> Original Title: <br>{{$movie['original_title']}}</h3>
+                <h4>Nationality: <br>{{strtoupper($movie['nationality'])}}</h4>
+                <h5>Release Date: <br>{{$movie['date']}}</h5>
                 <h6>Average Vote: {{$movie['vote']}}</h6>
             </div>
             
